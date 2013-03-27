3dbf0894e9aee55d63f236b8747578d1deda83d1
jackysee <jackysee@gmail.com>
2013-03-27 17:44:11 +0800

    clean up, move wrapper

--- src/qsa-wrapper.js
+++ src/qsa-wrapper.js
@@ -2,30 +2,21 @@ var QsaWrapper = {
 	slice: function(args){
 		return Array.prototype.slice.call(args);
 	},
-	extend: function(obj){
-		var sources = this.slice(arguments, 1);
-		for(var i=0, l=sources.length; i<l; i++){
-			var s = sources[i];
-			if(s){
-				for(var p in s){
-					obj[p] = s[p];
-				}
-			}
-		}
-		return obj;
+	wrap: function(inst, query, func){
+		return function(){
+			var args = QsaWrapper.slice(arguments);
+			[].forEach.call(query, function(node){
+				func.apply(node, args);
+			});
+			return inst;
+		};
 	},
 	create: function(methods){
 		return function(selector){
 			var query = document.querySelectorAll(selector);
 			var inst = {};
 			for(var m in methods){
-				inst[m] = function(){
-					var args = QsaWrapper.slice(arguments);
-					[].forEach.call(query, function(node){
-						methods[m].apply(node, args);
-					});
-					return inst;
-				};
+				inst[m] = QsaWrapper.wrap(inst, query, methods[m]);
 			}
 			return inst;
 		};
